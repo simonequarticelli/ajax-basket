6ab3d0d8c751721e372d48e30bcc7aac375de827
simonequarticelli <quarti87@gmail.com>
2019-05-22 17:56:24 +0200

    inizio esercizio ajax-basket

---
+++ main.js
@@ -0,0 +1,11 @@
+// ciao ragazzi, esercizio per oggi (repo: ajax-basket):
+// Utilizzare l’API: https://www.boolean.careers/api/array/basket?n=numberPlayers
+// Chiedere all’utente quanti giocatori vuole generare.
+// Chiedere all’API i giocatori e stampare a schermo una card per ogni giocatore attraverso handlebars.
+// Inizialmente chiamate l'API con un numero fisso stabilito da voi,
+// in modo da interrogare l'API nel modo corretto e stilare le card.
+// Poi aggiungete la richiesta del numero dei giocatori all'utente
+
+$(document).ready(function(){
+  console.log('ciao');
+});
